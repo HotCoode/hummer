229860587395f0834ecfdaf6e3430f47c6c467de
kai.wang <kai.wang.john@gmail.com>
2014-03-11 09:56:47 +0800

    添加setting功能

--- src/com/rise/fragment/SettingFragment.java
+++ src/com/rise/fragment/SettingFragment.java
@@ -1,6 +1,7 @@
 package com.rise.fragment;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
