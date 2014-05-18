61a9a6b924c831025bde23c5fc18e2d736e92739
John <kai.wang.john@gmail.com>
2014-05-18 20:51:52 +0800

    adjust sign in ui

---
+++ src/com/rise/http/AsyncHttp.java
@@ -0,0 +1,15 @@
+package com.rise.http;
+
+public class AsyncHttp {
+    private static final String BASE_URL = "http://xiiqi.com:8899/";
+    private static AsyncHttpClient client = new AsyncHttpClient();
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
