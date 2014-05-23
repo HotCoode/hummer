0ec250fd4a55df30bfed0e0a3a1c1d7a8efbb9b4
kai.wang <kai.wang.john@gmail.com>
2014-05-23 16:51:42 +0800

    clean source

--- src/com/rise/http/AsyncHttp.java
+++ src/com/rise/http/AsyncHttp.java
@@ -6,9 +6,6 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 public class AsyncHttp {
-//    private static final String BASE_URL = "http://xiiqi.com:8899/";
-
-    private static final String TEST_URL = "http://192.168.0.103:8000/r/api/";
 
     private static AsyncHttpClient client = new AsyncHttpClient();
 
@@ -21,7 +18,7 @@ public class AsyncHttp {
     }
 
     private static String getAbsoluteUrl(String relativeUrl) {
-        L.i("request " + TEST_URL + relativeUrl);
-        return TEST_URL + relativeUrl;
+        L.i("request " + Urls.BASE_URL + relativeUrl);
+        return Urls.BASE_URL + relativeUrl;
     }
 }
